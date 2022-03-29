b21593c4a19f82e42535030fe3a51a783c8fd4a4
Natanael Rolim <natanael.10@hotmail.com>
2022-03-29 12:07:25 -0300

    commit adição das novas funcionalidades REMOVER / ATUALIZAR / GET ALL

--- src/main/java/com/estudo/api/service/PessoaService.java
+++ src/main/java/com/estudo/api/service/PessoaService.java
@@ -3,6 +3,7 @@ package com.estudo.api.service;
 import com.estudo.api.entity.Pessoa;
 import com.estudo.api.entity.PessoaDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PessoaService {
@@ -10,4 +11,10 @@ public interface PessoaService {
     PessoaDto salvarPessoa(PessoaDto pessoaDto);
 
     Optional<Pessoa> retornarUmaPessoa(Long id);
+
+    List<Pessoa> retornarTodos();
+
+    void removeUmaPessoa(Long id);
+
+    PessoaDto atualizaUmaPessoa(PessoaDto pessoaDto);
 }
